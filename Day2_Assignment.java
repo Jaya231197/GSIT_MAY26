1add4525100906800abb253e4a7de5bf03512eb3
Jaya231197 <jayakeshri2397@gmail.com>
2026-06-26 14:21:04 +0530

    Add files via upload
    Day 1 and day 2 assignment

---
+++ Day2_Assignment.java
@@ -0,0 +1,30 @@
+package assignments;
+
+public class Day2_Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     char a ='A';
+     int value1 = a;
+		System.out.println("ASCII CODE :" + value1);
+		
+		char b ='a';
+	     int value2 = b;
+			System.out.println("ASCII CODE :" + value2);
+			
+			char  c='0';
+		     int value3 = c;
+				System.out.println("ASCII CODE :" + value3);
+				
+				char d ='@';
+			     int value4 = d;
+					System.out.println("ASCII CODE :" + value4);
+					
+					
+					
+					
+					
+		
+	}
+
+}
---
+++ Day2_AssignmentPrintTriangleusingprintln.java
@@ -0,0 +1,19 @@
+package assignments;
+
+public class Day2_AssignmentPrintTriangleusingprintln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+		double value =10.75 ;
+		int valueInt =(int)value;
+		System.out.println(valueInt);
+		System.out.println(value);
+	}
+
+}
